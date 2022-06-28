664e02d537b8513e5533f51444d4eca9c21f66a7
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-06-28 14:54:39 -0500

    payroll

---
+++ sat_companies_payroll/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': 'SAT COMPANIES PAYROLL',
+
+    'version': '14.0.0.0',
+
+    'author': "Process Control",
+
+    'contributors': ['Luis Felipe Paternina'],
+
+    'website': "https://www.processcontrol.es/",
+
+    'category': 'Payroll',
+
+    'depends': [
+
+        'contacts',
+        'hr'
+        'base',
+        'hr_payroll',
+        'base_automation',
+        'sat_companies',
+
+    ],
+
+    'data': [
+       
+        #'security/security.xml',
+        #'security/ir.model.access.csv',
+        #'views/res_company.xml',
+                   
+    ],
+    'installable': True
+}
