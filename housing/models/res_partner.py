7fb9163a2d6c19f6d0add685d8654484a7cff67b
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-06-30 08:19:12 -0500

    Update res_partner.py

--- housing/models/res_partner.py
+++ housing/models/res_partner.py
@@ -3,5 +3,6 @@ from odoo import models, fields, api, _
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    # CAMPOS
     age = fields.Integer(string="Age")
     gender = fields.Selection([('male','Male'),('famale','Famale')],string="Gender")
