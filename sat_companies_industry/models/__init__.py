b4721b5bc87c002ad724ab87f0bf9a62d3fed2ff
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-07-05 06:21:37 -0500

    new model

--- sat_companies_industry/models/__init__.py
+++ sat_companies_industry/models/__init__.py
@@ -8,3 +8,4 @@ from . import project_task_inspection
 from . import maintenance_type_deffect
 from . import project_task_ot_checklist_location
 from . import project_task_ot_checklist_minute
+from . import project_task_warning
