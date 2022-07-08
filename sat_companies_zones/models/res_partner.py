3e4a0bce70cc0c03e48f5442dd91447dad1b6413
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-07-08 04:54:18 -0500

    Delete res_partner.py

--- sat_companies_zones/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-   
-    
