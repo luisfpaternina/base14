ffceb397e3ccd73039c4a1057fe4cc322ef10cc1
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-07-20 16:27:01 -0500

    Update sale_order.py

--- sat_companies_sale/models/sale_order.py
+++ sat_companies_sale/models/sale_order.py
@@ -5,6 +5,7 @@ from odoo.exceptions import ValidationError
 from datetime import datetime, date
 import base64
 import logging
+from odoo.addons.mail.models import mail_template
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -91,6 +92,11 @@ class SaleOrder(models.Model):
     sign_contract_date = fields.Date(
         string="Sign contract date",
         compute="compute_contract_date")
+
+    def method_name(self): 
+        model = self.env['sale.type.service'].search([('id', 'in', self.type_service_id.id)])
+        converted_content = mail_template.mako_template_env.from_string(model.<htmlcontent>).render({'object': object_value,})
+        self.contract_ready = converted_content
 
 
     @api.depends('state','pdf_file_sale_contract')
