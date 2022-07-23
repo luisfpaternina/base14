6c0a59c9d3968754903fa9c644c0cfb3403af216
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-07-23 08:38:37 -0500

    Update __manifest__.py

--- sat_companies_industry/__manifest__.py
+++ sat_companies_industry/__manifest__.py
@@ -35,6 +35,7 @@
         'views/project_task_inspection.xml',
         'views/project_task_ot_checklist_location.xml',
         'views/project_task_breakdowns.xml',
+        'views/project_task_community_mood.xml',
         'reports/worksheet.xml',
         'reports/print_qr.xml',
         'reports/technical_data_template.xml',
